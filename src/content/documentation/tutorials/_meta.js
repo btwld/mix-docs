98b41a0d64f3cba930b4f9b3d044640e458a9206
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2026-03-25 00:31:11 -0300

    docs: Revise preview examples and registry (#878)

--- src/content/documentation/tutorials/_meta.js
+++ src/content/documentation/tutorials/_meta.js
@@ -2,5 +2,6 @@ export default {
     "creating-a-widget": "Creating a Widget",
     "controlling-widget-state": "Advanced Widget State Control",
     theming: "Theming",
-    "creating-context-variants": "Creating Context Variants"
+    "creating-context-variants": "Creating Context Variants",
+    "creating-custom-tokens": "Creating Custom Tokens"
 };
